e4ab62c6ca37d5e4ea44f52a3249123a0e445928
cjs <42282367@qq.com>
2016-09-18 21:01:08 +0800

    wx js

---
+++ fun.js
@@ -0,0 +1,11 @@
+
+//判断是否在微信内
+function is_weixin(){
+  var ua = navigator.userAgent.toLowerCase();
+  if(ua.match(/MicroMessenger/i)=="micromessenger") {
+    return true;
+   } else {
+    return false;
+  }
+}
+
